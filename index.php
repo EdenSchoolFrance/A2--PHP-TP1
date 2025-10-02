6449022ed686de8d22469f0a399b51e99ceb1b75
anxyum <abevigne@gmail.com>
2025-10-02 13:18:53 +0200

    Exercice 1

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>TP PHP 1</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
+    <p><?php $prenom = "Abel";
+    $age = 16;
+    echo "Je m'appelle $prenom et j'ai $age ans." ?></p>
 </body>
+
 </html>
