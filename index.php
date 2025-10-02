a5073109f4d0e40aee4464f04d22a6cd83aad5eb
Najaxx123 <romain.brival@outlook.fr>
2025-10-02 14:28:46 +0200

    add php simple condition

--- index.php
+++ index.php
@@ -11,8 +11,11 @@
 </html> -->
 
 <?php 
-function test($name, $lastname){
-    return $name." ".$lastname;
-};
+function test($age) {
+if ($age < "18") {
+    echo "<p>Je suis majeur(e)</p>";
+} else {
+    echo "<span>Je suis pas majeur(e)</span>";
+}};
 
-echo test("Romain", "Brival");
+echo test("19");
