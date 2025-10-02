b96023d8181d0e25bd2fc33a14ad777e6edbec97
Outrun <outrun.917@gmail.com>
2025-10-02 14:10:20 +0200

    add php simple condition

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>TP PHP 1</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
@@ -16,5 +16,17 @@
         echo "Je m'appelle $prenom et j'ai $age ans !";
         ?>
     </p>
+
+    <p <?php echo "class='$age >= 18 ? majeur : mineur'" ?>>
+        Test
+    </p>
+
+    <?php
+    if ($age >= 18) {
+        echo "<p class='green'>Je suis majeur(e)</p>";
+    } else {
+        echo "<p class='red'>Je suis mineur(e)</p>";
+    }
+    ?>
 </body>
 </html>
