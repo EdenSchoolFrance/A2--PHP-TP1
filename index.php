066100663cce6f0a9fb61a63fbb545700133ba70
Outrun <outrun.917@gmail.com>
2025-10-02 14:52:35 +0200

    add php simple array implementation

--- index.php
+++ index.php
@@ -8,25 +8,12 @@
 <body>
     <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
 
-    <p>
-        <?php 
-        $prenom = "Nolan";
-        $age = 17;
-
-        echo "Je m'appelle $prenom et j'ai $age ans !";
-        ?>
-    </p>
-
-    <p <?php echo "class='$age >= 18 ? majeur : mineur'" ?>>
-        Test
-    </p>
-
     <?php
-    if ($age >= 18) {
-        echo "<p class='green'>Je suis majeur(e)</p>";
-    } else {
-        echo "<p class='red'>Je suis mineur(e)</p>";
-    }
+        $prenoms = ["Nolan", "Raphaël", "Abel", "Warren", "Jahel"];
+
+        foreach($prenoms as $value) {
+            echo "<p>$value</p>";
+        }
     ?>
 </body>
 </html>
