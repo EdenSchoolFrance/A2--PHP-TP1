50296985c09c85446748a19c8de1cf3222ffb18f
chrismongombe50@gmail.com <chrismongombe50@gmail.com>
2025-10-02 14:00:57 +0200

    add file

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 </head>
 <body>
     <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
+
+    <?php $prénom = 'Mark'; $age = 20;?>
+
+    <p><?php echo "Son prénom est " ?><?php echo $prénom ?><?php echo " et il a " ?><?php echo $age ?><?php echo " ans." ?></p>
 </body>
 </html>
