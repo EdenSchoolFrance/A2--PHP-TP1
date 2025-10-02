513fe685ff72dda0d6a720317fd0805ec9b58cb9
rapha1005 <106275964+rapha1004@users.noreply.github.com>
2025-10-02 13:47:12 +0200

    add php simple array implementation

--- index.php
+++ index.php
@@ -7,14 +7,10 @@
 </head>
 <body>
     <?php
-    $prenom = "Raphael";
-    $age = 1;
-    echo "<h1>$prenom a $age ans</h1>";
-    if($age >= 18) {
-        echo "<h2 style='color: green';>Je suis majeur(e)</h2>";
-    }else {
-        echo "<h2 style='color: red';>Je suis mineur(e)</h2>";
-    }
+        $prenoms = ["Alice", "Bob", "Chloé", "David", "Emma"];
+        foreach($prenoms as $prenom) {
+            echo "<p>$prenom</p>";
+        }
     ?>
 </body>
 </html>
