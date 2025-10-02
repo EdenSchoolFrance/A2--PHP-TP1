91ecd3fc577f5a85d7b2f46389b9a5ccdf8e347a
ZuSaii <zusaiilegendary@gmail.com>
2025-10-02 14:42:11 +0200

    Exercice 5 Terminé

--- index.php
+++ index.php
@@ -7,5 +7,16 @@
 </head>
 <body>
     <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
+
+    <?php
+    
+    $users = ['Théo' => "22 ans", 'Raimon' => "22 ans", "Mathéo" => "16 ans"];
+    
+    foreach ($users as $name => $age) {
+        echo "<p>$name a $age</p>";
+    }
+
+    ?>
 </body>
+
 </html>
