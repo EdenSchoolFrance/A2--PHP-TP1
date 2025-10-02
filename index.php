8dd287d0ead1e54341c8da7e37a66411013a0ad3
Nohan-V2 <nseck@edenschool.fr>
2025-10-02 13:42:21 +0200

    add php simple condition

--- index.php
+++ index.php
@@ -6,6 +6,23 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
+    <?php
+
+    echo "<h1>Bienvenue dans mon premier TP PHP !</h1>";
+
+    $name = "Nohan";
+    $age = 17;
+
+    function YoungOrOld($age) {
+        if ($age >= 18) {
+            return "<p style='color: green'>Je suis majeur(e)</p>";
+        } else {
+            return "<p style='color: red'>Je suis majeur(e)</p>";
+        }
+    }
+
+    echo YoungOrOld($age);
+
+    ?>
 </body>
 </html>
