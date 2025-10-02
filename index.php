4dddc240a40f60460b7882fc30cd3f85ed870de6
Eden <yacinelahjaily@gmail.com>
2025-10-02 13:00:13 +0200

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>TP PHP 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
+</body>
+</html>
