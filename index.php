ff342620a64c50d64c63f7e4b54752f0c3073189
Nolann DOMAS <ndomas@edenschool.fr>
2025-10-02 13:58:48 +0200

    Mise en place de la fonction pour les couleurs en fonction de notre age et de la phrase de presentation

--- index.php
+++ index.php
@@ -2,10 +2,30 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>TP PHP 1</title>
-    <link rel="stylesheet" href="style.css">
+    <title><?php echo "TP PHP 1"; ?></title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
+    <?php 
+
+    $prenom = "Nolann";
+    $age = 16;
+
+    if ($age >= 18) {
+        $class = "major";
+        $minorOrMajor = "jesuis majeur";
+    } else {
+        $class = "minor";
+        $minorOrMajor = "je suis mineur";
+    }
+
+    ?>
+
+    <h1><?php echo "salut"; ?></h1>
+
+    <p > <?php echo "Je suis " . $prenom . " et j ai " . $age . " ans"; ?> </p>
+
+    <p class="<?php echo $class; ?>"><?php echo $minorOrMajor; ?></p>
+
 </body>
 </html>
