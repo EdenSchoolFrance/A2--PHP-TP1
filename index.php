e6534d432b4e0a9cf655fedcb82938cd76a43d40
NECR0M4NIA <gary.necromania@gmail.com>
2025-10-02 14:02:25 +0200

    add card for display user and age

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>TP PHP 1</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1><?php
-    $tab = ["Jean", "Marie", "Pierre", "Sophie", "Gary"];
-    foreach($tab as $value) {
-        echo "Current element of \$array: $value.<br>";
-    }
-    ?></h1>
+    <?php
+    $articles = [
+        "Apprendre PHP pas à pas",
+        "Faire sa première requête SQL",
+        "Créer sa première page web"
+    ];
+    ?>
+
+    <div class="card">
+
+    <h2>Mes articles</h2>
+    <ul>
+        <?php
+        foreach($articles as $article) {
+            echo "<li>$article</li>";
+        }
+        ?>
+    </ul>
+
+    </div>
 </body>
+
 </html>
