b663bac623c3d54b56220c1f769c7083904986c9
Warren Keumeugni <wkeumeumeugni@edenschool.fr>
2025-10-02 14:38:17 +0200

    add php simple condition

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>TP PHP 1</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
-</body>
-</html>
+<?php
+function test($word, $age) {
+    if($age >= 18) {
+
+
+
+        echo "<h2 style='color: green';>Je suis majeur(e)</h2>";
+
+
+    }else {
+
+
+        echo "<h2 style='color: red';>Je suis mineur(e)</h2>";
+
+
+    }
+    return "Hello $word tu as $age ans.";
+}
+echo test("John", 15)
+?>
