30d5f6f61c85c83230d49561283e0c1446ecffd4
Forcerion . <forcerion@gmail.com>
2025-10-02 13:51:05 +0200

    exo 1 finis

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>TP PHP 1</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
-</body>
-</html>
+<?php 
+$age = 17;
+$prenom = "Victor";
+
+echo "$prenom a bientôt $age ans !";
+?>
