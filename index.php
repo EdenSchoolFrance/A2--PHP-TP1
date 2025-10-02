239d8ec0ce0be6cba3505a033972d9887724e2a2
Nolann DOMAS <ndomas@edenschool.fr>
2025-10-02 14:07:00 +0200

    add php simple array implementation

--- index.php
+++ index.php
@@ -25,7 +25,19 @@
 
     <p > <?php echo "Je suis " . $prenom . " et j ai " . $age . " ans"; ?> </p>
 
-    <p class="<?php echo $class; ?>"><?php echo $minorOrMajor; ?></p>
+    <p class="<?php $class; ?>"><?php echo $minorOrMajor; ?></p>
+
+    <?php 
+
+    $prenoms = ["PafMan", "Asteflix", "Asterion", "George", "Norman"];
+
+    
+    foreach ($prenoms as $prenoms) {
+        echo "<h3>$prenoms</h3>";
+    }
+
+
+    ?>
 
 </body>
 </html>
