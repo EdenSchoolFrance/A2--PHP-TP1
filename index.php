e00cec1b809279ccac5ab765f2c8dfe8c008f5f6
MrRacoonGH <hadrien181107@gmail.com>
2025-10-02 14:59:09 +0200

    wip

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>TP PHP 1</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1><?php echo "Bienvenue dans mon premier TP PHP !"; ?></h1>
+    <?php
+        $prenom = ['Hadrien ']
+        $age = 18;
+    ?>
 </body>
 </html>
